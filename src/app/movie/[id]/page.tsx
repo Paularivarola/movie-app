6c67c2819bf593fe055aee29c9ee8b2f7c8a8f39
Paula Rivarola <paularivarola@MBP-de-Paula.fibertel.com.ar>
2024-06-23 20:16:55 -0300

    fix: Corrected ESLint errors in Header

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -5,17 +5,17 @@ import { useParams } from 'next/navigation';
 import MovieDetails from '../../components/MovieDetails/MovieDetails';
 
 function MoviePage() {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    if (!id) {
-        return <div>Loading...</div>;
-    }
+  if (!id) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-        <div>
-            <MovieDetails movieId={id as string} />
-        </div>
-    );
+  return (
+    <div>
+      <MovieDetails movieId={id as string} />
+    </div>
+  );
 }
 
 export default MoviePage;
